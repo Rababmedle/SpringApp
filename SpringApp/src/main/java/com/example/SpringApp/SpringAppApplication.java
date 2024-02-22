4b616e9cd39d17b021a5f38ce870e352ac57fd03
Rababmedle <rabab.elmamy@gmail.com>
2024-02-22 18:47:13 +0100

    for jenkins

--- SpringApp/src/main/java/com/example/SpringApp/SpringAppApplication.java
+++ SpringApp/src/main/java/com/example/SpringApp/SpringAppApplication.java
@@ -1,13 +1,13 @@
-package com.example.BnidarkApp;
+package com.example.SpringApp;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class  BnidarkAppApplication {
+public class SpringAppApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(BnidarkAppApplication.class, args);
+		SpringApplication.run(SpringAppApplication.class, args);
 	}
 
 }
--- SpringApp/src/test/java/com/example/SpringApp/SpringAppApplicationTests.java
+++ SpringApp/src/test/java/com/example/SpringApp/SpringAppApplicationTests.java
@@ -1,10 +1,10 @@
-package com.example.BnidarkApp.BnidarkApp;
+package com.example.SpringApp;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class BnidarkAppApplicationTests {
+class SpringAppApplicationTests {
 
 	@Test
 	void contextLoads() {
